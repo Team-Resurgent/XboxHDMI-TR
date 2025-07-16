732b753c3bf42d8f98337b19c3c68fc8f6667cb4
EqUiNoX <equinox.teamresurgent@gmail.com>
2025-07-15 18:29:13 -0600

    Added: HD+ support

--- src/interrupt.c
+++ src/interrupt.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #include "stm32f0xx_hal.h"
-#include "stm32f0xx.h"
+#include "main.h"
 #include "adv7511.h"
 #include "interrupt.h"
 
@@ -15,10 +15,10 @@ void SysTick_Handler(void)
     HAL_SYSTICK_IRQHandler();
 }
 
-void EXTI4_15_IRQHandler(void)
+void ADV_IRQ_HANDLER(void)
 {
     encoder.interrupt = 1;
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_9);
+    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
 }
 
 void NMI_Handler(void)
--- src/main.h
+++ src/main.h
@@ -4,11 +4,6 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-#define ADV7511_INT_Pin GPIO_PIN_9
-#define ADV7511_INT_GPIO_Port GPIOA
-#define ADV7511_INT_EXTI_IRQn EXTI4_15_IRQn
-#define STATUS_LED GPIO_PIN_10
-
 #ifdef __cplusplus
 extern "C"
 {

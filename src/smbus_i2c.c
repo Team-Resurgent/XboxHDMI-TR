0b35d08be233177563968eaa2ef9d133c4fd27a5
EqUiNoX <equinox.teamresurgent@gmail.com>
2025-11-15 12:50:43 -0700

    Update smbus_i2c.c

--- src/smbus_i2c.c
+++ src/smbus_i2c.c
@@ -5,11 +5,19 @@
 #define I2C_SLAVE_ADDR 0x69
 
 I2C_HandleTypeDef hi2c2;
-uint8_t rx_data[2];     // [0] = command, [1] = data
+uint8_t rx_data[2];     // [0] = command, [1] = data (if applicable)
 uint8_t tx_data[256];   // Response data indexed by command
+uint8_t last_command = 0; // Store last command for read operations
+uint8_t rx_count = 0;   // Track how many bytes received
 
 void smbus_i2c_init()
 {
+    // Initialize all registers with dummy value 0x69
+    for (int i = 0; i < 256; i++)
+    {
+        tx_data[i] = 0x69;
+    }
+    
     __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_I2C2_CLK_ENABLE();
 
@@ -24,7 +32,7 @@ void smbus_i2c_init()
 
     // Configure I2C2 as slave
     hi2c2.Instance = I2C2;
-    hi2c2.Init.Timing = 0x2000090E;
+    hi2c2.Init.Timing = 0x00303D5B;
     hi2c2.Init.OwnAddress1 = (I2C_SLAVE_ADDR << 1);
     hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
     hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
@@ -32,6 +40,10 @@ void smbus_i2c_init()
     hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
     
     HAL_I2C_Init(&hi2c2);
+    
+    // Enable I2C2 interrupt in NVIC
+    HAL_NVIC_SetPriority(I2C2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(I2C2_IRQn);
     
     // Start listening for address match
     HAL_I2C_EnableListen_IT(&hi2c2);
@@ -45,34 +57,61 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, ui
     if (TransferDirection == I2C_DIRECTION_TRANSMIT)  // Master writing to us
     {
         debug_log("I2C ADDR: Master Write\r\n");
-        HAL_I2C_Slave_Seq_Receive_IT(hi2c, rx_data, 2, I2C_FIRST_AND_LAST_FRAME);
+        rx_count = 0;
+        // Receive 1 byte at a time using NEXT_FRAME to allow continued reception
+        HAL_I2C_Slave_Seq_Receive_IT(hi2c, &rx_data[rx_count], 1, I2C_NEXT_FRAME);
     }
-    else  // Master reading from us
+    else  // Master reading from us (Xbox read: read data from last command)
     {
-        debug_log("I2C ADDR: Master Read\r\n");
-        HAL_I2C_Slave_Seq_Transmit_IT(hi2c, tx_data, 1, I2C_FIRST_AND_LAST_FRAME);
+        debug_log("I2C ADDR: Master Read (CMD=0x%02X)\r\n", last_command);
+        // Send data corresponding to the last received command
+        HAL_I2C_Slave_Seq_Transmit_IT(hi2c, &tx_data[last_command], 1, I2C_FIRST_AND_LAST_FRAME);
     }
 }
 
-// Called when we finish receiving data (command + data)
+// Called when we finish receiving data
 void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
 {
-    uint8_t command = rx_data[0];
-    uint8_t data = rx_data[1];
+    debug_log("I2C RX: Byte[%d]=0x%02X\r\n", rx_count, rx_data[rx_count]);
     
-    debug_log("I2C RX: CMD=0x%02X DATA=0x%02X\r\n", command, data);
+    rx_count++;
     
-    // Store received data for later read-back
-    tx_data[command] = data;
+    // If we have room for more bytes, prepare to receive next byte
+    if (rx_count < 2)
+    {
+        HAL_I2C_Slave_Seq_Receive_IT(hi2c, &rx_data[rx_count], 1, I2C_NEXT_FRAME);
+    }
+}
+
+// Called when master sends STOP or repeated START
+void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    debug_log("I2C Listen Complete, rx_count=%d\r\n", rx_count);
     
-    // Listen for next transaction
+    if (rx_count == 1)
+    {
+        // Received 1 byte (command only) - Xbox read command
+        last_command = rx_data[0];
+        debug_log("I2C: Read command for register 0x%02X\r\n", last_command);
+    }
+    else if (rx_count == 2)
+    {
+        // Received 2 bytes (command + data) - Write command
+        uint8_t command = rx_data[0];
+        uint8_t data = rx_data[1];
+        tx_data[command] = data;
+        last_command = command;
+        debug_log("I2C: Write 0x%02X to register 0x%02X\r\n", data, command);
+    }
+    
+    rx_count = 0;
     HAL_I2C_EnableListen_IT(hi2c);
 }
 
 // Called when we finish transmitting data
 void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c)
 {
-    debug_log("I2C TX: 0x%02X sent\r\n", tx_data[0]);
+    debug_log("I2C TX: CMD=0x%02X DATA=0x%02X sent\r\n", last_command, tx_data[last_command]);
     HAL_I2C_EnableListen_IT(hi2c);
 }
 

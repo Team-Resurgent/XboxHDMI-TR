fc63fa7738b82778f94ef7b96f6d684338092918
edlf <93866317+edlf@users.noreply.github.com>
2025-12-25 14:30:17 +0100

    Prevent interlaced from impacting all modes

--- src/main.c
+++ src/main.c
@@ -338,7 +338,7 @@ void set_video_mode_bios(const uint32_t mode, const uint32_t avinfo, const video
     // TODO: Figure out if the avinfo is a reliable source for the interlaced flag
     // const bool interlaced = mode_index == 0x0e;
     // most modes are progressive on the bus...
-    const bool interlaced = avinfo & XBOX_AVINFO_INTERLACED;
+    const bool interlaced = (avinfo & XBOX_AVINFO_INTERLACED) && (mode_index == 0x0e);
 
     if (interlaced) {
         video_mode.v_active = video_mode.v_active / 2;

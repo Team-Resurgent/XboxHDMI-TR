2765c5e552212bae1f9bd6743e2cd9b895a478b1
EqUiNoX <equinox.teamresurgent@gmail.com>
2026-01-01 15:41:51 -0700

    Update interrupt.c

--- src/application/interrupt.c
+++ src/application/interrupt.c
@@ -1,4 +1,5 @@
 #include "stm32f0xx_hal.h"
+#include "adv7511.h"
 #include "..\shared\debug.h"
 
 void SysTick_Handler(void)
@@ -11,4 +12,12 @@ void HardFault_Handler(void)
 {
     debug_log("HardFault occured!\n");
     while (1);
+}
+
+extern adv7511 encoder;
+
+void ADV_IRQ_HANDLER(void)
+{
+    encoder.interrupt = 1;
+    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
 }

22208c38fb70d3c8d7e5e13c59402c078cdd687e
EqUiNoX <equinox.teamresurgent@gmail.com>
2026-01-02 11:14:11 -0700

    improved debug to reduce mem when not used

--- src/shared/debug.h
+++ src/shared/debug.h
@@ -1,6 +1,19 @@
 #pragma once
 
+//#define DEBUG_OUT
+
+#ifdef DEBUG_OUT
+
 void debug_init();
 void debug_log(const char *fmt, ...);
 void debug_ring_log(const char *fmt, ...);
 void debug_ring_flush();
+
+#else
+
+#define debug_init()                     ((void)0)
+#define debug_log(...)                   ((void)0)
+#define debug_ring_log(...)              ((void)0)
+#define debug_ring_flush()               ((void)0)
+
+#endif
